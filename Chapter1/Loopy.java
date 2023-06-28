87102194cc73de955ecbd35d7b755ffe31170023
andrusha2 <herr.dron@mail.ru>
2023-06-28 21:39:19 +0300

    updated

--- Chapter1/Loopy.java
+++ Chapter1/Loopy.java
@@ -6,7 +6,7 @@ public class Loopy {
         System.out.println("Before the loop");
         while (x < 4) {
             System.out.println("In the loop");
-            System.out.println("Value of x is " + x);
+            System.out.println("Value of x is "  + x);
             x += 1;
         }
         System.out.println("This is after the loop");
