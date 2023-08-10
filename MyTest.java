d3db1078d7cdbdaed4b679beb3db64e49b1fd23c
andrusha2 <herr.dron@mail.ru>
2023-08-10 13:07:29 +0300

    10.08.2023

--- MyTest.java
+++ MyTest.java
@@ -1,8 +1,27 @@
+import java.util.ArrayList;
+
 public class MyTest {
     public static void main(String[] args){
-        int x = 5;
-        System.out.println(x);
-        x = 6;
-        System.out.println(x + 1);
+        String[] myList = new String[2];
+        String a = new String("whoohoo");
+        myList[0] = a;
+
+        String b = new String("Frog");
+        myList[1] = b;
+
+        int theSize = myList.length;
+
+        Object o = myList[1];
+
+        myList[1] = null;
+
+        boolean isIn = false;
+
+        for (String i:myList){
+            if (b.equals(i)) {
+                isIn = true;
+                break;
+            }
+        }
     }
 }
